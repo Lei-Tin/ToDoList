2facfee08f3832d345d51cbbc39821f59f05518d
HLeiTR <sy.hung@mail.utoronto.ca>
2023-01-06 22:31:51 -0500

    [+] Added strikethrough for tasks completed

--- tasks.js
+++ tasks.js
@@ -2,15 +2,15 @@ let inputField = document.getElementById("input-task");
 let addTaskButton = document.getElementById("add-task-button");
 
 // Disabling the button at the start
-// addTaskButton.disabled = true;
+addTaskButton.disabled = true;
 
 function validateInput() {
     if (inputField.value.length > 0) {
-        // addTaskButton.disabled = false;
+        addTaskButton.disabled = false;
         addTaskButton.style.backgroundColor = "rgba(100, 149, 237, 1)";
         addTaskButton.style.cursor = "pointer";
     } else {
-        // addTaskButton.disabled = true;
+        addTaskButton.disabled = true;
         addTaskButton.style.backgroundColor = "rgba(100, 149, 237, 0.5)";
         addTaskButton.style.cursor = "not-allowed";
     }
@@ -38,14 +38,12 @@ function addTask() {
     const item = inputField.value;
 
     const input = document.createElement('input');
+    input.setAttribute('onclick', 'strikeThrough(this)')
     input.setAttribute('class', 'task-checkbox');
     input.setAttribute('type', 'checkbox');
     input.setAttribute('aria-label', 'Checkbox for item');
 
     li.appendChild(input);
-
-    const para = document.createElement('p');
-    li.append(para);
 
     const span = document.createElement('span');
     span.setAttribute('class', 'task');
@@ -68,6 +66,14 @@ function addTask() {
 function deleteTask(btnItem) {
     // After some testing, I realized that btnItem.parentNode gives me the list item
     btnItem.parentNode.remove();
+}
+
+function strikeThrough(checkBoxItem) {
+    if (checkBoxItem.checked) {
+        checkBoxItem.nextSibling.classList.add('strike-through')
+    } else {
+        checkBoxItem.nextSibling.classList.remove('strike-through')
+    }
 }
 
 addTaskButton.addEventListener('click', addTask)
